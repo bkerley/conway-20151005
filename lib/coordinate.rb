fa4d2810413b694b4effaee92adfa55394ac4ebc
Bryce Kerley <bkerley@brycekerley.net>
2015-10-05 17:32:07 -0400

    implement coordinate neighbors

--- lib/coordinate.rb
+++ lib/coordinate.rb
@@ -1,8 +1,18 @@
+require 'set'
+
 class Coordinate
   attr_accessor :x, :y
 
   def initialize(x, y)
     @x = x
     @y = y
+  end
+
+  def neighbors
+    x_options = [x-1, x, x+1]
+    y_options = [y-1, y, y+1]
+
+    all_coords = Set.new(x_options.product y_options)
+    all_coords.delete [x, y]
   end
 end
