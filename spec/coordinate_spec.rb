ab492536c6bec25b3d0499683430940b452daa2a
Bryce Kerley <bkerley@brycekerley.net>
2015-10-05 17:21:25 -0400

    coordinate knows where it is

--- spec/coordinate_spec.rb
+++ spec/coordinate_spec.rb
@@ -1,6 +1,14 @@
 require_relative '../lib/coordinate'
 
 describe Coordinate do
+  let(:x){rand(100_000)}
+  let(:y){rand(100_000)}
+
+  subject{ described_class.new x, y }
+
   it 'finds a set of neighbors'
-  it 'has its own location'
+  it 'has its own location' do
+    expect(subject.x).to eq x
+    expect(subject.y).to eq y
+  end
 end
