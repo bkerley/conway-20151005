0d0de033cab0b5e5c2d4473666587b3a8b0abadc
Bryce Kerley <bkerley@brycekerley.net>
2015-10-05 17:55:35 -0400

    specced out cell rules

---
+++ spec/cell_spec.rb
@@ -0,0 +1,33 @@
+require_relative '../lib/cell'
+
+describe Cell do
+  describe 'when alive' do
+    subject{ described_class.new :alive }
+
+    [0, 1, 4, 5, 6, 7, 8].each do |live_neighbors|
+      it "dies with #{live_neighbors} live neighbors" do
+        expect(subject.next(live_neighbors)).to_not be_alive
+      end
+    end
+
+    [2, 3].each do |live_neighbors|
+      it "lives with #{live_neighbors} live neighbors" do
+        expect(subject.next(live_neighbors)).to be_alive
+      end
+    end
+  end
+
+  describe 'when dead' do
+    subject{ described_class.new :dead }
+
+    [0, 1, 2, 4, 5, 6, 7, 8].each do |live_neighbors|
+      it "dies with #{live_neighbors} live neighbors" do
+        expect(subject.next(live_neighbors)).to_not be_alive
+      end
+    end
+
+    it "lives with 3 live neighbors" do
+      expect(subject.next(3)).to be_alive
+    end
+  end
+end
